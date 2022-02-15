d822e6ecd7f01df6e27092d943c5070c744e1ee7
CelesteGeremia1 <97689830+CelesteGeremia1@users.noreply.github.com>
2022-02-15 09:11:19 -0800

    Add files via upload

---
+++ Unit000/src/AsciiBox.java
@@ -0,0 +1,25 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Celeste Geremia \t  January 18th 2022 \n\n" );
+		System.out.println("____________________ " );
+		System.out.println("|                  | " );
+		System.out.println("|                  | " );
+		System.out.println("|                  | " );
+		System.out.println("|                  | " );
+		System.out.println("|                  | " );
+		System.out.println("|                  | " );
+		System.out.println("|                  | " );
+		System.out.println("|__________________| " );
+
+	}
+}
---
+++ Unit000/src/Variables.java
@@ -0,0 +1,64 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = -3424;
+		int intOne = 32006; 
+		long longOne = 99988999; 
+
+		//decimal variables
+		float floatOne = (float) 38.56;
+		double doubleOne = 876.547;
+
+		//other integer types
+		char charOne = 'A';
+
+		//other types
+		boolean booleanOne = true;
+		String stringOne = "hello people";
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*Integer variables \t\t*");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t\t*");
+		System.out.println("*Decimal variables\t\t*");
+		System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*");
+		System.out.println("*Other Integer Types\t\t*");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*Other Types\t\t*");
+		System.out.println("*booleanOne = "+booleanOne+"\t\t*");
+		System.out.println("*stringOne = "+stringOne+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
